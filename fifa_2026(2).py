623ced23262247090b79bf4029bcfa928b6a24c3
amirsa-uni <130916925+amirsa-uni@users.noreply.github.com>
2023-04-17 17:29:43 +0100

    Update fifa_2026(2).py

--- fifa_2026(2).py
+++ fifa_2026(2).py
@@ -18,16 +18,9 @@ from sklearn.metrics import accuracy_score
 from sklearn import metrics
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression, Ridge
-
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.activations import relu,linear
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.optimizers import Adam
 import math
 import warnings
 warnings.filterwarnings('ignore')
